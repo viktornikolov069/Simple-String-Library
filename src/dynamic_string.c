a1a9432cab68a1936b91ab3894b5eec7e3a00b87
Viktor Nikolov <viktor.nikolov069@gmail.com>
2023-07-28 20:33:13 +0300

    * Made changes to init_dynamic_string function * Added a tests folder for fututre unit tests

--- src/dynamic_string.c
+++ src/dynamic_string.c
@@ -1,5 +1,6 @@
 #include "dynamic_string.h"
 #include <string.h>
+#include <stdio.h>
 
 size_t get_size_private(const DynamicString_t *self) {
     return self->size;
@@ -38,14 +39,23 @@ void dest_str_private(DynamicString_t *str) {
 
 DynamicString_t* init_dynamic_string(const char *str) {
     DynamicString_t* new_str = (DynamicString_t*)malloc(sizeof(DynamicString_t));
-    new_str->self = new_str;
-    new_str->size = strlen(str);
 
-    char* data = (char*)calloc(new_str->size, sizeof(char));
-    strcpy(data, str);
-    new_str->data = data;
+    new_str->self = new_str;
+
+    // Add one for the null terminator
+    new_str->size = strlen(str) + 1;
+
+    new_str->data = (char*)calloc(new_str->size, sizeof(char));
+    strncpy(new_str->data, str, new_str->size);
+
+    //strncpy probably copied the null terminator as well in
+    //the previous step but just in case I'll set it again.
     new_str->data[new_str->size] = '\0';
 
+    //Assigning the func. pointers to the 
+    //actual functions which are purposely marked as private.
+    //The idea is to simulate having member functions 
+    //which isn't possible in C.
     new_str->get_size = get_size_private;
     new_str->get_str_immutable = get_str_immutable_private;
     new_str->dest_str = dest_str_private;
