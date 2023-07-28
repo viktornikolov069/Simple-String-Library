0d6db8875bff3fe9f82b741746cb9d7cf3badbdf
Viktor Nikolov <viktor.nikolov069@gmail.com>
2023-07-28 19:08:07 +0300

    Added NULL assignment after freeing memory in dest_str_private

--- include/dynamic_string.h
+++ include/dynamic_string.h
@@ -3,7 +3,8 @@
 
 #include <stdlib.h>
 
-#define SELF const struct DynamicString*
+#define SELF_CONST const struct DynamicString*
+#define SELF struct DynamicString*
 
 typedef struct DynamicString {
     
@@ -11,20 +12,20 @@ typedef struct DynamicString {
     char* data;
     size_t size;
 
-    size_t (*get_size)(SELF);
-    const char* (*get_str_immutable)(SELF);
+    size_t (*get_size)(SELF_CONST);
+    const char* (*get_str_immutable)(SELF_CONST);
     void (*dest_str)(SELF);
-    struct DynamicString*(*copy)(SELF);
+    struct DynamicString*(*copy)(SELF_CONST);
 
 } DynamicString_t;
 
-size_t get_size_private(SELF);
+size_t get_size_private(SELF_CONST);
 
-const char* get_str_immutable_private(SELF);
+const char* get_str_immutable_private(SELF_CONST);
 
 void dest_str_private(SELF);
 
-DynamicString_t* copy_private(SELF);
+DynamicString_t* copy_private(SELF_CONST);
 
 DynamicString_t* init_dynamic_string(const char *str);
 
--- src/dynamic_string.c
+++ src/dynamic_string.c
@@ -1,7 +1,5 @@
 #include "dynamic_string.h"
-
 #include <string.h>
-#include <stdio.h>
 
 size_t get_size_private(const DynamicString_t *self) {
     return self->size;
@@ -11,21 +9,31 @@ const char* get_str_immutable_private(const DynamicString_t *self) {
     return self->data;
 }
 
-DynamicString_t* copy(const DynamicString_t *self) {
+DynamicString_t* copy_private(const DynamicString_t *self) {
     DynamicString_t* copy_str = (DynamicString_t*)malloc(sizeof(DynamicString_t));
-    memcpy(copy_str, self, sizeof(DynamicString_t));
-    copy_str->data = (char*)calloc(self->size, sizeof(char));
 
+    //Shallow copy
+    memcpy(copy_str, self, sizeof(DynamicString_t));
+
+    //Deep copy
+    copy_str->data = (char*)calloc(self->size, sizeof(char));
     memcpy(copy_str->data, self->data, self->size);
     copy_str->data[self->size] = '\0';
 
+    //Point to self
     copy_str->self = copy_str;
     return copy_str;
 }
 
-void dest_str_private(const DynamicString_t *str) {
+void dest_str_private(DynamicString_t *str) {
+    if (str == NULL) {
+        return;
+    }
     free(str->data);
     free(str->self);
+    str->data = NULL;
+    str->self = NULL;
+    str = NULL;
 }
 
 DynamicString_t* init_dynamic_string(const char *str) {
@@ -41,7 +49,7 @@ DynamicString_t* init_dynamic_string(const char *str) {
     new_str->get_size = get_size_private;
     new_str->get_str_immutable = get_str_immutable_private;
     new_str->dest_str = dest_str_private;
-    new_str->copy = copy;
+    new_str->copy = copy_private;
 
     return new_str;
 }
